1f27c022d0006dc67ec59fa08172fc25cdefb252
Tord.Monclair <tord.monclair@student.uib.no>
2023-12-14 01:47:50 +0100

    Updates

---
+++ assets/Page-785af281.js
@@ -0,0 +1,1 @@
+import{N as a,F as s}from"./Footer-f1be3867.js";import{d as n,b as r,o as c,c as m,e,a as o,F as _}from"./index-2fc7337e.js";const i={class:"container flex-shrink-0 mb-4"},l={class:"container footer mb-2 mt-auto"},v=n({__name:"Page",setup(p){return(f,u)=>{const t=r("router-view");return c(),m(_,null,[e(a),o("main",i,[e(t)]),o("footer",l,[e(s)])],64)}}});export{v as default};
--- assets/Page-cf455729.js
+++
@@ -1,1 +0,0 @@
-import{N as a,F as s}from"./Footer-d180fe0e.js";import{d as n,b as r,o as c,c as m,e,a as o,F as _}from"./index-f0e710d9.js";const i={class:"container flex-shrink-0 mb-4"},l={class:"container footer mb-2 mt-auto"},v=n({__name:"Page",setup(p){return(f,u)=>{const t=r("router-view");return c(),m(_,null,[e(a),o("main",i,[e(t)]),o("footer",l,[e(s)])],64)}}});export{v as default};
