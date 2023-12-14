43444ac58efdbda43943cbe8002d7e22d116fb70
Tord.Monclair <tord.monclair@student.uib.no>
2023-12-14 02:42:05 +0100

    Updates

--- assets/Page-0bff625b.js
+++
@@ -1,1 +0,0 @@
-import{N as a,F as s}from"./Footer-54cd765a.js";import{d as n,b as r,o as c,c as m,e,a as o,F as _}from"./index-4b9182d6.js";const i={class:"container flex-shrink-0 mb-4"},l={class:"container footer mb-2 mt-auto"},v=n({__name:"Page",setup(p){return(f,u)=>{const t=r("router-view");return c(),m(_,null,[e(a),o("main",i,[e(t)]),o("footer",l,[e(s)])],64)}}});export{v as default};
