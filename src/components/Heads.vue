799a007b30db543d09791307452be059f25f00eb
tordbm <tord.monclair@student.uib.no>
2023-12-19 00:12:36 +0100

    moved icon to src

--- src/components/Heads.vue
+++ src/components/Heads.vue
@@ -2,7 +2,7 @@
         <div class="container-fluid py-4">
             <h1 class="display-6  fw-bold">
                 <slot></slot>
-                <img src="public/favicon.ico" alt="Sun behind cloud" height="100">
+                <img src="@/icons/partially-sunny.png" alt="Sun behind cloud" height="100">
             </h1>
     </div>
 </template>
