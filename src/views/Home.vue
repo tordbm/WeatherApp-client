c8ae1e96a58d657724ebc22d17ae39e1d1523bc8
tordbm <tordbm@icloud.com>
2024-01-29 23:18:58 +0100

    minor changes to icon layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,7 @@
     <div class="col-md-5 mb-3">
       <div class="p-5 box border rounded-3">
         <label for="inputCity" class="form-label">Last fetch was for {{ lastCity }}: {{ lastFetch }}</label>
+        <div class="input-group mb-3">
         <input 
         v-model="city" 
         type="input"
@@ -14,12 +15,13 @@
         class="form-control"
         @keyup.enter="fetchData"
         :disabled="loading">
-        <button type="button" class="btn btn-outline-success mt-2" @click="fetchData">Submit</button>
-        <button type="button" class="btn btn-no-outline ml-5" @click="getLocation()">
-          <svg class="pa-0 ma-0" width="20" height="20" fill="red" viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg">
-            <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+        <button type="button" class="btn btn-no-outline" @click="getLocation()">
+          <svg class="pa-0 ma-0" width="20" height="20" fill="#d40000" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M256 0c17.7 0 32 14.3 32 32V66.7C368.4 80.1 431.9 143.6 445.3 224H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H445.3C431.9 368.4 368.4 431.9 288 445.3V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V445.3C143.6 431.9 80.1 368.4 66.7 288H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H66.7C80.1 143.6 143.6 80.1 224 66.7V32c0-17.7 14.3-32 32-32zM128 256a128 128 0 1 0 256 0 128 128 0 1 0 -256 0zm128-80a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"/>
           </svg>
         </button>
+        <button type="button" class="btn btn-outline-primary" @click="fetchData">Submit</button>
+        </div>
       </div>
     </div>
     
