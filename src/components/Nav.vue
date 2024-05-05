e67bd61ebebaa7d3e6958059f742cb3dd43fe2b5
tordbm <tordbm@icloud.com>
2024-05-05 23:20:10 +0200

    fix navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,45 +1,47 @@
 <template>
-  <nav
-    class="navbar navbar-expand-lg mb-4 border-bottom"
+  <div
+    class="navbar navbar-expand-lg border-bottom mb-4"
     style="background-color: azure">
-    <router-link
-      :to="routes[0].path"
-      class="navbar-brand d-flex align-items-center text-decoration-none">
-      <img
-        src="@/icons/partially-sunny.png"
-        alt="Sun behind cloud"
-        height="50" />
-      World Wide Weather
-    </router-link>
-    <button
-      class="navbar-toggler border-0 shadow-none"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <nav class="container">
+      <router-link
+        :to="routes[0].path"
+        class="navbar-brand d-flex align-items-center">
+        <img
+          src="@/icons/partially-sunny.png"
+          alt="Sun behind cloud"
+          height="50" />
+        World Wide Weather
+      </router-link>
+      <button
+        class="navbar-toggler border-0 shadow-none"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ms-auto">
-        <li
-          class="nav-item text-uppercase"
-          v-for="route in routes"
-          :key="route.path">
-          <router-link
-            :to="route.path"
-            class="nav-link"
-            :title="route.children[0].name"
-            :class="{ active: isActive(route.path) }">
-            <i class="bi bi-house-fill" v-if="route.path === `/`"></i>
-            {{ route.path !== `/` ? route.children[0].name : '' }}
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+          <li
+            class="nav-item text-uppercase"
+            v-for="route in routes"
+            :key="route.path">
+            <router-link
+              :to="route.path"
+              class="nav-link"
+              :title="route.children[0].name"
+              :class="{ active: isActive(route.path) }">
+              <i class="bi bi-house-fill" v-if="route.path === `/`"></i>
+              {{ route.path !== `/` ? route.children[0].name : '' }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script lang="ts" setup>
